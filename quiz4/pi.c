2884c65fd97bc8aaada6431af2a9f432b354ec34
rageNami <gr7p4zxh@gmail.com>
2021-05-27 16:32:04 +0800

    Measure calculation time

--- quiz4/pi.c
+++ quiz4/pi.c
@@ -1,8 +1,9 @@
+#include "threadpool.c"
 #include <math.h>
 #include <stdio.h>
-#include "threadpool.c"
 
 #define PRECISION 100 /* upper bound in BPP sum */
+#define ONE_SEC 1000000000.0
 
 /* Use Bailey–Borwein–Plouffe formula to approximate PI */
 static void *bpp(void *arg) {
@@ -18,6 +19,9 @@ static void *bpp(void *arg) {
 int main() {
   int bpp_args[PRECISION + 1];
   double bpp_sum = 0;
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
   tpool_t pool = tpool_create(4);
   tpool_future_t futures[PRECISION + 1];
 
@@ -34,6 +38,12 @@ int main() {
   }
 
   tpool_join(pool);
+
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  printf("Elapsed time: %.0f ns\n",
+         (double)(end.tv_sec - start.tv_sec) * ONE_SEC +
+             (end.tv_nsec - start.tv_nsec));
   printf("PI calculated with %d terms: %.15f\n", PRECISION + 1, bpp_sum);
   return 0;
 }
